f9c71a8b6cc1fa7d35f2c8434cdf7b047b698c2a
Túlio Marinho <tulio.smarinho@gmail.com>
2025-05-25 22:15:45 -0300

    implementation

---
+++ Cesar-Cypher.py
@@ -0,0 +1,13 @@
+
+print("inser the message")
+message = input("message")
+shift = 1
+
+
+def cypher():
+    encrypted = ""
+    for char in message:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            encrypted += chr((ord(char) - base + shift) % 26 + base)
+
