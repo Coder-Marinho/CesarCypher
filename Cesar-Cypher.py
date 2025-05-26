131d540954ae2b1d209fa0f5c809ad2673fe50ae
Túlio Marinho <tulio.smarinho@gmail.com>
2025-05-25 22:19:41 -0300

    implementation of the key of cypher

--- Cesar-Cypher.py
+++ Cesar-Cypher.py
@@ -1,8 +1,7 @@
 
 print("inser the message")
 message = input("message")
-shift = 1
-
+shift = input("enter with the key of cypher")
 
 def cypher():
     encrypted = ""
@@ -10,4 +9,9 @@ def cypher():
         if char.isalpha():
             base = ord('A') if char.isupper() else ord('a')
             encrypted += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            encrypted += char  # Mantém espaços, pontuação etc.
+    return encrypted
 
+encrypted_message = cypher(message, shift)
+print("Encrypted message:", encrypted_message)
